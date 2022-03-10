8a585e5693e88018e713599c339fc19dc06b09d3
aaron <aaron@RIG>
2022-03-09 21:20:54 -0500

    Add project files.

---
+++ Sandbox/Data/SandboxContext.cs
@@ -0,0 +1,20 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Sandbox.Models;
+
+namespace Sandbox.Data
+{
+    public class SandboxContext : DbContext
+    {
+        public SandboxContext (DbContextOptions<SandboxContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Sandbox.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ Sandbox/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sandbox.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
